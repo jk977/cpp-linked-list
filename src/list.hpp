4f9cf1c6f1ac74291cbc2fe0f5f5473016004406
jk977 <jdk64@comcast.net>
2018-10-31 23:56:55 -0700

    Implementing removal functions

--- src/list.hpp
+++ src/list.hpp
@@ -21,21 +21,23 @@ public:
 
     void push_front(T val);
     void push_back(T val);
-    void insert(std::size_t index, T val);
+    void insert(T val, std::size_t index);
 
-    T&& pop_front();
-    T&& pop_back();
+    std::optional<T> pop_front();
+    std::optional<T> pop_back();
     void remove(std::size_t index);
 
     std::optional<T> get_front() const;
-    std::optional<T> get_last() const;
+    std::optional<T> get_back() const;
     std::optional<T> get(std::size_t index) const;
 
     std::size_t length() const;
     bool empty() const;
 
 private:
+    std::optional< list_node<T>* > node_at(std::size_t index) const;
     void insert_empty(T val);
+    void insert_middle(T val, std::size_t index);
 
     std::size_t m_length;
     list_node<T>* m_sentinel;
@@ -113,10 +115,83 @@ void list<T>::push_back(T val) {
 }
 
 template<class T>
+std::optional< list_node<T>* > node_from_offset(std::size_t index, list_node<T>* start) {
+    if (start == nullptr) {
+        return std::nullopt;
+    } else if (index == 0) {
+        return std::make_optional(start);
+    }
+
+    return node_from_offset(index-1, start->next);
+}
+
+template<class T>
+std::optional< list_node<T>* > list<T>::node_at(std::size_t index) const {
+    return node_from_offset(index, m_sentinel->next);
+}
+
+template<class T>
+void list<T>::insert_middle(T val, std::size_t index) {
+    auto new_node = new list_node(val);
+    auto target = *node_at(index);
+    auto prev = target->prev;
+
+    prev->next = new_node;
+    new_node->prev = prev;
+
+    new_node->next = target;
+    target->prev = new_node;
+}
+
+template<class T>
+void list<T>::insert(T val, std::size_t index) {
+    if (index == 0) {
+        push_front(val);
+    } else if (index == length()) {
+        push_back(val);
+    } else {
+        insert_middle(val, index);
+    }
+}
+
+template<class T>
 std::optional<T> value_of(list_node<T>* node) {
     return (node != nullptr) ?
         std::make_optional(node->value) :
         std::nullopt;
+}
+
+template<class T>
+std::optional<T> pop(list_node<T>* node) {
+    if (node == nullptr) {
+        return std::nullopt;
+    }
+
+    auto node_before = node->prev;
+    auto node_after = node->next;
+    auto value = value_of(node);
+
+    node->next = nullptr;
+    node_before->next = node_after;
+    node_after->prev = node_before;
+    delete node;
+
+    return value;
+}
+
+template<class T>
+std::optional<T> list<T>::pop_front() {
+    return pop(m_sentinel->next);
+}
+
+template<class T>
+std::optional<T> list<T>::pop_back() {
+    return pop(m_sentinel->prev);
+}
+
+template<class T>
+void list<T>::remove(std::size_t index) {
+    pop( node_at(index).value_or(nullptr) );
 }
 
 template<class T>
@@ -125,21 +200,13 @@ std::optional<T> list<T>::get_front() const {
 }
 
 template<class T>
-std::optional<T> list<T>::get_last() const {
+std::optional<T> list<T>::get_back() const {
     return value_of(m_sentinel->prev);
 }
 
 template<class T>
 std::optional<T> list<T>::get(std::size_t index) const {
-    auto current = m_sentinel->next;
-    std::size_t i = 0;
-
-    while (current != nullptr && i < index) {
-        current = current->next;
-        i++;
-    }
-
-    return value_of(current);
+    return value_of( node_at(index).value_or(nullptr) );
 }
 
 template<class T>

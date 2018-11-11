e2a3f1f5a2e22fcfbf3cc408819c3eb818de9645
jk977 <jdk64@comcast.net>
2018-11-11 12:25:52 -0800

    Inlining list node functions

--- src/list_node.hpp
+++ src/list_node.hpp
@@ -4,21 +4,12 @@
 template<class T>
 struct list_node {
     list_node() = default;
-    list_node(T val);
-    ~list_node();
+    list_node(T val): value(val) {}
+    ~list_node() { delete next; }
 
     T value;
     list_node<T>* prev;
     list_node<T>* next;
 };
-
-template<class T>
-list_node<T>::list_node(T val): value(val)
-{}
-
-template<class T>
-list_node<T>::~list_node() {
-    delete next;
-}
 
 #endif // LIST_NODE_HPP_

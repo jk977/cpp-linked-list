6b5fe8d75afd362acbf02b7f49de6aefb150de4a
jk977 <jdk64@comcast.net>
2018-11-11 17:30:32 -0800

    Red test (multithreading failure)

--- test/list.cpp
+++ test/list.cpp
@@ -3,6 +3,10 @@
 
 #include "../src/list.hpp"
 
+#include <functional>
+#include <thread>
+#include <vector>
+
 using dsa::list;
 
 BOOST_AUTO_TEST_CASE(iterators) {
@@ -172,5 +176,36 @@ BOOST_AUTO_TEST_CASE(clear) {
 
     l.clear();
     BOOST_TEST( length(l) == 0 );
+}
+
+BOOST_AUTO_TEST_CASE(multithreading) {
+    list<int> l;
+
+    for (int i = 0; i < 100; i++) {
+        l.push_back(i);
+    }
+
+    auto length = [](auto& l) { return (int) l.length(); };
+
+    auto process_list = [&length](list<int>& l) {
+        for (int i = 0; i < length(l); i++) {
+            ++l[i];
+        }
+    };
+
+    int const thread_count = 500;
+    std::vector<std::thread> threads(thread_count);
+
+    for (int i = 0; i < thread_count; i++) {
+        threads[i] = std::thread(process_list, std::ref(l));
+    }
+
+    for (int i = 0; i < thread_count; i++) {
+        threads[i].join();
+    }
+
+    for (int i = 0; i < length(l); i++) {
+        BOOST_TEST( l[i] == (thread_count + i) );
+    }
 }
 
